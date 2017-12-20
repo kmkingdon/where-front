b5402ee91001d52dff1c72ed7c1d08a6723d3b28
KEVIN OBRIEN <kevinobrien@KEVINs-MacBook-Air.local>
2017-12-20 14:59:28 -0700

    it populates

--- JS/list.js
+++ JS/list.js
@@ -3,86 +3,61 @@ const form = document.querySelector("form");
 const url = "https://where-server-2.herokuapp.com/events";
 const sceneChoice = document.querySelector(".scene-choice");
 const pickCost = document.querySelector(".pick-cost");
-var eventsArray;
-var chosenScene;
-var chosenCost;
-
-console.log(form)
 
 form.addEventListener("submit", pressButton);
 
-
-
 function pressButton (event){
   event.preventDefault();
-  console.log("the function is running");
-  chosenScene = sceneChoice.value;
-  chosenCost = pickCost.value;
+  localStorage.setItem('scene', event.target[0].value);
+  localStorage.setItem('cost', event.target[1].value);
+  location.href = "eventlist.html";
+}
+
+if(location.href.split("/")[3] === "eventlist.html") {
   getData();
-  populateList();
 }
 
 function getData (){
   fetch (url)
     .then((resp) => resp.json())
-    .then(function (resp){
-      eventsArray = resp.events
-        .catch(console.error);
-    });
+    .then(fetchdata)
+    .catch(console.error);
 }
+function fetchdata(resp){
+  let scene= localStorage.getItem("scene");
+  let cost= localStorage.getItem("cost");
+  let sceneArray= []
+  let finalArray= []
 
-function populateList (){
-  for (var i= 0; i < eventsArray.length; i++){
-    if (eventsArray[i].scene === chosenScene && eventsArray[i].cost === chosenCost){
-      var newEventCard = document.createElement("div");
-      var eventName = document.createElement("h1");
-      eventName.innerText = eventsArray[i].eventname;
-      newEventCard.appendChild(eventName);
-      var eventLocation = document.createElement("h2");
-      eventLocation.innerText = eventsArray[i].eventLocation;
-      newEventCard.appendChild(eventLocation);
-      var time = document.createElement("h3");
-      time.innerText = eventsArray[i].time;
-      newEventCard.appendChild(time);
-      var cost = document.createElement("h3");
-      cost.innerText = eventsArray[i].price;
-      newEventCard.appendChild(cost);
-      var link = document.createElement("h3");
-      link.innerText = eventsArray[i].link;
-      newEventCard.appendChild(link);
-      eventList.appendChild(newEventCard);
+
+  for (var i= 0; i < resp.length; i++){
+    if (resp[i].scene === scene){
+      sceneArray.push(resp[i])
     }
   }
-}
 
-function saveQuestionsList() {
-  for (var i = 0; i < localStorage.length; i++) {
-    let listQuestion= document.createElement('li');
-    let remove= document.createElement('span');
-    remove.innerHTML = "remove";
-    remove.className += 'remove ';
-    remove.id +=  i;
-    remove.addEventListener('click', removeQuestion);
-    listQuestion.innerHTML= localStorage.getItem(i+1);
-    listQuestion.appendChild(remove);
-    listQuestion.setAttribute('id', i);
-    favoriteList.appendChild(listQuestion);
+  for (var i = 0; i < sceneArray.length; i++) {
+    if(sceneArray[i].cost.toString() === cost)
+      finalArray.push(sceneArray[i])
   }
-}
 
-saveQuestionsList();
-
-function removeQuestion(event){
-  let questionID= event.target.id;
-  questionToRemove= document.getElementById(questionID);
-  favoriteList.removeChild(questionToRemove);
-}
-
-reset.addEventListener('click', resetList);
-
-function resetList() {
-  while (favoriteList.hasChildNodes()) {
-      favoriteList.removeChild(favoriteList.lastChild);
+  for (var i = 0; i < finalArray.length; i++) {
+    var newEventCard = document.createElement("div");
+    var eventName = document.createElement("h1");
+    eventName.innerText = finalArray[i].eventname;
+    newEventCard.appendChild(eventName);
+    var eventLocation = document.createElement("h2");
+    eventLocation.innerText = finalArray[i].eventLocation;
+    newEventCard.appendChild(eventLocation);
+    var time = document.createElement("h3");
+    time.innerText = finalArray[i].time;
+    newEventCard.appendChild(time);
+    var costDisplay = document.createElement("h3");
+    costDisplay.innerText = finalArray[i].price;
+    newEventCard.appendChild(costDisplay);
+    var link = document.createElement("h3");
+    link.innerText = finalArray[i].link;
+    newEventCard.appendChild(link);
+    eventList.appendChild(newEventCard);
   }
-  localStorage.clear();
 }
